e5d76418b2d0fa91aa24f507408b498fb66cd197
salmi3d <salmi3d@gmail.com>
2019-08-31 09:58:04 +0300

    basic stuff

---
+++ app.js
@@ -0,0 +1,25 @@
+class App {
+
+  constructor() {
+    this.init()
+  }
+
+  init() {
+    console.log('init')
+  }
+
+  animate() {
+
+  }
+
+  render() {
+
+  }
+
+  resize() {
+
+  }
+
+}
+
+new App()
