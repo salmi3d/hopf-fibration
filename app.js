f95176d4e34b40f8091ad75cfcd7777d540b76c6
salmi3d <salmi3d@gmail.com>
2019-09-07 10:17:42 +0300

    first implementation

--- app.js
+++ app.js
@@ -1,23 +1,58 @@
+import * as THREE from 'three'
+const OrbitControls = require('three-orbit-controls')(THREE)
+
 class App {
 
   constructor() {
+    this.width = window.innerWidth
+    this.height = window.innerHeight
+    this.vCount = 50
+    this.groupCount = 3
+    this.etaStep = 0.1
+    this.rotationSpeed = 0.0005
+
     this.init()
   }
 
   init() {
-    console.log('init')
+    this.scene = new THREE.Scene()
+    this.renderer = new THREE.WebGLRenderer()
+
+    this.renderer.setPixelRatio(window.devicePixelRatio)
+    this.renderer.setSize(this.width, this.height)
+    this.container = document.getElementById('app')
+    this.container.appendChild(this.renderer.domElement)
+
+    this.camera = new THREE.PerspectiveCamera(65, this.width / this.height, 1, 1000)
+    this.camera.position.set(-30, -50, -50)
+
+    this.spotLight = new THREE.SpotLight(0xffffff)
+    this.spotLight.position.set(30, 50, 50)
+    this.spotLight.castShadow = true
+    this.spotLight.intensity = 3
+    this.scene.add(this.spotLight)
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+
+    window.addEventListener('resize', this.resize.bind(this))
+    this.resize()
+    this.animation()
   }
 
-  animate() {
-
+  animation() {
+    this.scene.rotateZ(this.rotationSpeed)
+    requestAnimationFrame(this.animation.bind(this))
+    this.render()
   }
 
   render() {
-
+    this.renderer.render(this.scene, this.camera)
   }
 
   resize() {
-
+    this.renderer.setSize(this.width, this.height)
+    this.camera.aspect = this.width / this.height
+    this.camera.updateProjectionMatrix()
   }
 
 }
